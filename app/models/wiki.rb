a86b047d2bc217729b6b5ddbf9f2dcc7ca32e490
Brock Cooper <brockcooper@gmail.com>
2015-06-01 06:55:55 -0600

    Working on Models

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,6 +1,4 @@
 class Wiki < ActiveRecord::Base
   has_many :collaborators
   has_many :users, through: :collaborators
-  belongs_to :owner, :class_name => "User" 
-  belongs_to :creator, :class_name => "User" 
 end
--- db/migrate/20150601115124_create_wikis.rb
+++ db/migrate/20150601115124_create_wikis.rb
@@ -4,13 +4,8 @@ class CreateWikis < ActiveRecord::Migration
       t.string :title
       t.text :body
       t.boolean :private
-      
-      t.belongs_to :owner
-      t.belongs_to :creator
 
       t.timestamps null: false
     end
-      add_foreign_key :wikis, column: :owner_id
-      add_foreign_key :wikis, column: :creator_id
   end
 end
